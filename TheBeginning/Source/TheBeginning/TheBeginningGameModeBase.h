3399df62049796e82b3585668b53c1b99aafd86a
yotta <mojingyan233@gmail.com>
2017-06-23 18:32:10 +0800

    commit 1

---
+++ TheBeginning/Source/TheBeginning/TheBeginningGameModeBase.h
@@ -0,0 +1,19 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "GameFramework/GameModeBase.h"
+#include "TheBeginningGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class THEBEGINNING_API ATheBeginningGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
