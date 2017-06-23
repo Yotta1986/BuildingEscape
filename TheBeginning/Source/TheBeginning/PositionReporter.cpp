7b688a630f32164420ca15d8bfa45473b627abf8
yotta <mojingyan233@gmail.com>
2017-06-23 21:50:16 +0800

    commit 3

---
+++ TheBeginning/Source/TheBeginning/PositionReporter.cpp
@@ -0,0 +1,41 @@
+// Copyright yotta 2017
+
+#include "TheBeginning.h"
+#include "PositionReporter.h"
+
+
+// Sets default values for this component's properties
+UPositionReporter::UPositionReporter()
+{
+	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
+	// off to improve performance if you don't need them.
+	PrimaryComponentTick.bCanEverTick = true;
+
+	// ...
+}
+
+
+// Called when the game starts
+void UPositionReporter::BeginPlay()
+{
+	Super::BeginPlay();
+
+	FString name = GetOwner()->GetName();
+	FString pos = GetOwner()->GetActorLocation().ToString();
+
+	UE_LOG(LogTemp, Warning, TEXT("%s is at %s"), *name, *pos);
+	
+
+	// ...
+	
+}
+
+
+// Called every frame
+void UPositionReporter::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	// ...
+}
+
